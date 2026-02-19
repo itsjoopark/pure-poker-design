4f0089fba19a4535769bf87a859fc5fd618dcd7b
Jules <hjoo.edu@gmail.com>
2026-02-18 19:59:03 -0500

    refactor: strip page to interactive ASCII background only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import AsciiBackground from '@/components/AsciiBackground';
-import Navbar from '@/components/Navbar';
-import HeroSection from '@/components/HeroSection';
 
 export default function Home() {
   return (
     <main className="relative min-h-screen">
       <AsciiBackground />
-      <Navbar />
-      <HeroSection />
     </main>
   );
 }
